1a982af29206ca193c0dc539181636911165c67e
Ildar Gabdrakhmanov <13336584+ildarcheg@users.noreply.github.com>
2026-05-20 12:36:35 -0500

    test(cli): Layer B fixture sanity for box_with_text.stl (T3 split)

--- tests/cli/unit/test_split_fixtures.cpp
+++ tests/cli/unit/test_split_fixtures.cpp
@@ -26,3 +26,19 @@ TEST_CASE("two_cubes.stl is two-component (Layer A fixture sanity)",
     INFO("connected components: " << components.size());
     REQUIRE(components.size() == 2);
 }
+
+TEST_CASE("box_with_text.stl is multi-component (Layer B fixture sanity)",
+          "[orca-cli][split][fixture]") {
+    const auto path = fs::path(ORCA_CLI_STL_DIR) / "box_with_text.stl";
+    if (!fs::exists(path)) {
+        SKIP("box_with_text.stl not present at " << path.string()
+             << " - copy it from C:/Users/ildarcheg/Documents/GitHub/");
+    }
+    Slic3r::Model m;
+    REQUIRE(Slic3r::load_stl(path.string().c_str(), &m, nullptr));
+    REQUIRE(m.objects.size() == 1);
+    REQUIRE(m.objects[0]->volumes.size() == 1);
+    auto components = m.objects[0]->volumes[0]->mesh().split();
+    INFO("connected components: " << components.size());
+    REQUIRE(components.size() >= 2);
+}
